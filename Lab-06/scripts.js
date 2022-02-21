c6f438ea7660e24c3bc765b0f5d1101a05dca283
Suraj Odedra <suraj699@icloud.com>
2022-02-21 14:48:33 +0000

    Some work done on lab-06

---
+++ Lab-06/scripts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function addItem(text) {
+  const item = document.createElement('li');
+  item.textContent = text;
+  todo.appendChild(item);
+}
+
+add.addEventListener('click', ev => {
+  addItem(text.value);
+});
